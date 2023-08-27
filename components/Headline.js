edd1e5629333af41539df62ff63848055e891f78
takazo3 <info@re-zero-jp.com>
2023-08-27 13:34:58 +0900

    💄 スタイルファイルの分割

--- components/Headline.js
+++ components/Headline.js
@@ -1,10 +1,10 @@
-import styles from "../styles/Home.module.css";
+import classes from "./styles/Headline.module.css";
 
 export function Headline(props) {
   return (
     <>
-      <h1 className={styles.title}>{props.page} page</h1>
-      <p className={styles.description}>Get started by editing {props.children}</p>
+      <h1 className={classes.title}>{props.page} page</h1>
+      <p className={classes.description}>Get started by editing {props.children}</p>
     </>
   );
 }
