eeac4295536fb9dd11c30d3be3d74f50fa35e893
takazo3 <info@re-zero-jp.com>
2023-08-29 23:35:18 +0900

    ✨ ヘッダーの追加

---
+++ components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Link from "next/link";
+import classes from "./styles/Header.module.css";
+
+export function Header() {
+  return (
+    <header className={classes.header}>
+      <Link href="/" className={classes.link}>Index</Link>
+      <Link href="/about" className={classes.link}>About</Link>
+    </header>
+  );
+}
